1a391ab61cc2e78056eea7c8df8ec3b1c2cb5f9c
Lakrandi-Ayeshani <lakrandiayeshani1204@gmail.com>
2023-10-04 22:11:13 +0530

    change mobile view

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -16,24 +16,24 @@ const NewsCard = ({articles, setClickedArticle}) => {
 
   return (
     <div className='container'>
-      <Row xs={1} md={2} className="g-4">
+      <Row xs={1} lg={2} className="g-4">
       {articles.map((article, index) => (
         article.title !== "[Removed]" && (
         <Col key={index} onClick={() => handleClick(article)}>
         <Card  className='border-5 m-3 news-card'>
           <Card.Body>
-            <Card.Title className='text-start'>{article.title}</Card.Title>
+            <Card.Title className='text-start'>{index} {article.title}</Card.Title>
             <div >
               <Card.Text className='text-start m-0'>{article.source.name} - {article?.author }</Card.Text>
               <p className='text-start date-text'>{article.publishedAt.slice(0,10)}   {article.publishedAt.slice(11,16)}</p>
             </div>
             {article.urlToImage ? (
               <Row>
-                <Col className='d-flex justify-content-center'>
-                  <Card.Img src={article.urlToImage}></Card.Img>
+                <Col className='d-flex justify-content-center' sm={5}>
+                  <Card.Img src={article.urlToImage} alt="Image Loading.." className='text-center alt'></Card.Img>
                 </Col>
                 <Col>
-                  <Card.Text className='text-start'>{article.description}</Card.Text>
+                  <Card.Text className='text-start' sm={7}>{article.description}</Card.Text>
                 </Col>
               </Row>
             ) : (
